6c9746c53b0d9571256ddcc9fa795876299ab9b6
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-11 07:36:53 +0200

    feat: add previewSchedule helper func

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 const { addTalk } = require('./addTalk');
 const { removeLastTalk } = require('./removeLastTalk');
 const { undoRemoveTalk } = require('./undoRemoveTalk');
+const { previewSchedule } = require('./previewSchedule');
 
-module.exports = { addTalk, removeLastTalk, undoRemoveTalk };
+module.exports = { addTalk, removeLastTalk, undoRemoveTalk, previewSchedule };
