d8bebab73a93dcf4a4e28934f9a7142bb3394d9b
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-11 07:18:05 +0200

    feat: add init state and basic state management

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+const { bold, blueBright } = require('chalk');
 const { APP_VERSION } = require('./constants');
 
 function init() {
   console.log(
-    `\nWelcome to Conference Track Manager${
-      APP_VERSION ? ` v${APP_VERSION}` : ''
-    }!`,
+    bold(
+      blueBright(
+        `\nWelcome to Conference Track Manager${
+          APP_VERSION ? ` v${APP_VERSION}` : ''
+        }!`,
+      ),
+    ),
   );
 
   return 0;
---
+++ src/state/initState.js
@@ -0,0 +1,16 @@
+const { BASE_TALK_START_TIME } = require('../constants');
+
+const initState = Object.freeze({
+  completedTrackNum: 0,
+  currentTrackComplete: false,
+  currentTrackDuration: 0,
+  currentTrackNum: 1,
+  lunchEaten: false,
+  maxEveningTalkRemaining: 240,
+  maxMorningTalkRemaining: 180,
+  morningComplete: false,
+  nextTalkStartTime: BASE_TALK_START_TIME,
+  talks: [],
+});
+
+module.exports = { initState };
