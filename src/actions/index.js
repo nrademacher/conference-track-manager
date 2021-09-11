bb03c2f0e79631083bbe1ec26c07fa7e24f4b1ae
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-11 07:26:37 +0200

    feat: add action funcs for removing talks/undoing

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+const { addTalk } = require('./addTalk');
+const { removeLastTalk } = require('./removeLastTalk');
+const { undoRemoveTalk } = require('./undoRemoveTalk');
+
+module.exports = { addTalk, removeLastTalk, undoRemoveTalk };
