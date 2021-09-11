5b7bc8d66fe61c021b414e00c80d561c859f504e
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-11 07:34:33 +0200

    feat: add buildTable helper func

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 const { displayTrackStatsMsg } = require('./displayTrackStatsMsg');
 const { getActionChoice } = require('./getActionChoice');
+const { buildTable } = require('./buildTable');
 
-module.exports = { displayTrackStatsMsg, getActionChoice };
+module.exports = { displayTrackStatsMsg, getActionChoice, buildTable };
