503d97b6d21b3abe3c3bec7f18abb8b18c715677
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-12 21:28:16 +0200

    feat: add isState method to StateMape to support resolve method

--- src/state/stateMap.js
+++ src/state/stateMap.js
@@ -1,26 +1,33 @@
 class StateMap extends Map {
   constructor(state) {
     super();
-    this.initState = Object.freeze(state);
+    this.initState = Object.freeze({ ...state });
     this.set('default', this.initState);
     this.set('current', this.initState);
   }
 
-  chain(newState, key = Date.now(), oldState = this.get('current')) {
-    this.set(key, { ...oldState });
+  isState(obj) {
+    return (
+      Object.keys(obj).length === Object.keys(this.initState).length &&
+      Object.keys(obj).every((key) => this.initState.hasOwnProperty(key))
+    );
+  }
+
+  chain(newState, key = Date.now(), prevState = this.get('current')) {
+    this.set(key, { ...prevState });
     this.set('previous', key);
     this.set('current', { ...newState });
 
-    return { newState, key, oldState };
+    return newState;
   }
 
   resolve(key) {
     const res = this.get(key);
 
-    if (typeof res === 'object') {
+    if (this.isState(res)) {
       return res;
     }
-    if (res || Number.isInteger(res)) {
+    if (this.has(res)) {
       return this.resolve(res);
     }
     return false;
